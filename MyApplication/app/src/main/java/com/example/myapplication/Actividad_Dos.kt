00bc6d1b120d178cf9899c0438706b0706b5f671
Washio1995 <wacho_jax@hotmail.com>
2019-05-17 15:58:42 -0500

    Intent Explicitos

--- MyApplication/app/src/main/java/com/example/myapplication/Actividad_Dos.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/Actividad_Dos.kt
@@ -3,17 +3,32 @@ package com.example.myapplication
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_actividad__dos.*
 
 class Actividad_Dos : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actividad__dos)
+
+
+        val nombre: String? = intent.getStringExtra("nombre")
+        val edad: Int? = intent.getIntExtra("edad", 0)
+
+        Log.i("intents", "Nombre: $nombre")
+        Log.i("intents", "Edad: $edad")
+
+        btn_actividad_1.setOnClickListener {
+            irAActividadUno()
+        }
+
     }
 
-    fun irAActividadTres() {
-        val intent = Intent(this, Actividad_Dos::class.java)
-        startActivity(intent)
+    fun irAActividadUno() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent
+        )
     }
 
 
