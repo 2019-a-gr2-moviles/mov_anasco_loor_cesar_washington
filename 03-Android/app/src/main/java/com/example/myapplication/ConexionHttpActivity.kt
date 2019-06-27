7b5270b6dd7efd8c7b947676e9663dd7c07fc2c2
Washington Añasco <wacho_jax@hotmail.com>
2019-06-27 16:07:48 -0500

    Fluel con Sails

--- 03-Android/app/src/main/java/com/example/myapplication/ConexionHttpActivity.kt
+++ 03-Android/app/src/main/java/com/example/myapplication/ConexionHttpActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import com.beust.klaxon.Klaxon
 import java.lang.Exception
 import java.util.*
+import com.github.kittinunf.result.Result.*
+import com.github.kittinunf.fuel.httpGet
 
 class ConexionHttpActivity : AppCompatActivity() {
 
@@ -35,28 +37,51 @@ class ConexionHttpActivity : AppCompatActivity() {
     """.trimIndent()
 
         try {
-            val empresaInstancia= Klaxon().parseArray<Empresa>(json)
+            val empresaInstancia = Klaxon().parseArray<Empresa>(json)
 
             empresaInstancia?.forEach {
                 Log.i("http", "Nombre: ${it.nombre}")
                 Log.i("http", "Id: ${it.id}")
                 Log.i("http", "Fecha: ${it.fechaCreacion}")
                 it.usuariosDeEmpresa.forEach {
-                    Log.i("http",
-                        "Nombre: ${it.nombre}")
-                    Log.i("http",
-                        "FK: ${it.fkEmpresa}")
+                    Log.i(
+                        "http",
+                        "Nombre: ${it.nombre}"
+                    )
+                    Log.i(
+                        "http",
+                        "FK: ${it.fkEmpresa}"
+                    )
                 }
 
             }
-        }catch (e:Exception){
+        } catch (e: Exception) {
             Log.i("http", "Error instanciado de la empresa")
 
         }
 
+        val url = "http://172.31.104.114:1337/empresa/1"
+
+        url
+            .httpGet()
+            .responseString { request, response, result ->
+                when (result) {
+                    is Failure -> {
+                        val ex = result.getException()
+                        Log.i("http", "Error: ${ex.message}")
+                    }
+                    is Success -> {
+                        val data = result.get()
+                        Log.i("http", "Data: ${data}")
+
+
+
+                    }
+                }
+
+
+            }
+
+
     }
-
-
-
-
 }
